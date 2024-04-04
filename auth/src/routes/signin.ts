2ccd8095619db11185fb6f602e82ac923e1d67a0
Piyushkriplani444 <piyushkriplani444@gmail.com>
2024-04-04 16:40:29 +0530

    created a common repository

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,10 +1,8 @@
 import express, { Request, Response } from "express";
-import { body, validationResult } from "express-validator";
-import { RequestValidationError } from "../errors/request-validation-error";
-import { validateRequest } from "../middlewares/validate-request";
+import { body } from "express-validator";
+import { validateRequest, BadRequestError } from "@pkorga/common";
 import { User } from "../models/user";
 import { Password } from "../services/password";
-import { BadRequestError } from "../errors/bad-request-errror";
 import jwt from "jsonwebtoken";
 
 const router = express.Router();
