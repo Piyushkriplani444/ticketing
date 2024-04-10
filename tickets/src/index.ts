050ada99e65acb3066ee9af919a34274ba133a27
Piyushkriplani444 <piyushkriplani444@gmail.com>
2024-04-10 19:36:28 +0530

    added ticket server routes

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,9 +1,13 @@
 import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
-import { errorHandler, NotFoundError } from "@pkorga/common";
+import { errorHandler, NotFoundError, currentUser } from "@pkorga/common";
 import mongoose from "mongoose";
 import cookieSession from "cookie-session";
+import { createTicketRouter } from "./routes/new";
+import { showTicketRouter } from "./routes/show";
+import { indexTicketRouter } from "./routes";
+import { updateTicketRouter } from "./routes/update";
 
 const app = express();
 app.set("trust proxy", true);
@@ -15,9 +19,16 @@ app.use(
   })
 );
 
+app.use(currentUser);
+
+app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
 app.all("*", async (req, res, next) => {
   throw new NotFoundError();
 });
+
 app.use(errorHandler);
 
 const start = async () => {
